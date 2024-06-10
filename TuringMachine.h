b94faf82aff900c22f629c507f404a283899e5d8
star-candy <el0710@naver.com>
2024-06-11 00:29:19 +0900

    table class 기본 요구사항 작성

--- TuringMachine.h
+++ TuringMachine.h
@@ -11,19 +11,19 @@
 
 namespace Turing
 {
-	const char EMPTY_SYMBOL = '_';
-	const char WILDCARD_SYMBOL = '*';
+	const char EMPTY_SYMBOL = '_'; //비어있음
+	const char WILDCARD_SYMBOL = '*'; //와일드카드
 
 	enum class Move
 	{
-		NONE = 0, LEFT, RIGHT
+		NONE = 0, LEFT, RIGHT // 이동 상태 정의
 	};
 
 	struct Transition
 	{
 	public:
 		Transition(const std::string& curr_s, char read_s, char write_s, Move m, const std::string& next_s);
-
+		//현재 상태, 테잎에서 읽을 값, 작성할 값, 이동할 방향, 다음 상태 : 5가지에 대한 테이블 상태
 		void print(std::ostream& os) const;
 
 		inline const std::string& getCurrState() const { return curr_state; }
@@ -42,13 +42,28 @@ namespace Turing
 
 	class Table
 	{
+	public:
+		Table();
+		~Table();
+
+		void addTransition(const std::string& curr_s, char read_s, char write_s, Move move, const std::string& next_s);
+		Transition* findTransition(const std::string& curr_s, char read_s);
+		void clear();
+		void print(std::ostream& os) const;
+		void initialize(const std::string& rule_script);
+		bool load(const std::string& path);
+
+	private:
+		//추가
 	};
 
 	class Tape
 	{
+		//기초 함수 정의는 과제 pdf에 제시됨, 선행 class 제작 후 구현할 것
 	};
 
 	class Machine
 	{
+		//기초 함수 정의는 과제 pdf에 제시됨 선행 class 제작 후 구현할 것
 	};
 };
